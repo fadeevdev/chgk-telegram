c32abeb7367073534102034c5b8c6677d46a8e94
dmitrii.fadeev <fadeevdev@gmail.com>
2022-05-10 18:51:44 +0300

    test

--- internal/app/telegram/telegram.go
+++ internal/app/telegram/telegram.go
@@ -30,7 +30,10 @@ func (c *Client) SendMessage(chatID uint64, message string) error {
 	`, chatID, message)
 
 	resp, err := cl.Post(url, "application/json", bytes.NewBuffer([]byte(payload)))
-	fmt.Println("resp", resp)
+
+	if err != nil {
+		return err
+	}
 	if resp.StatusCode != http.StatusOK {
 		return errors.New(resp.Status)
 	}
