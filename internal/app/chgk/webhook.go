08d7d39540414898a7d7afbf941160faa5711ba8
dmitrii.fadeev <fadeevdev@gmail.com>
2022-06-01 09:55:22 +0300

    printing all received webhoom messages to stdio

--- internal/app/chgk/webhook.go
+++ internal/app/chgk/webhook.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (s *chgkServer) WebHook(ctx context.Context, message *pb.Message) (*pb.Empty, error) {
-	fmt.Printf("received a message: %s\n", message.Text)
+	fmt.Printf("received a message: %s\n", message)
 	switch message.Text {
 	case "/start":
 		_, err := s.RegisterUser(ctx, message.From)
