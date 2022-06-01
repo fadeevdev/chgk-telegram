18089c84f4cce2355f6e5c54f31e6dcb0b23c696
dmitrii.fadeev <fadeevdev@gmail.com>
2022-06-01 10:36:34 +0300

    webhoom start command functionality

--- internal/app/chgk/webhook.go
+++ internal/app/chgk/webhook.go
@@ -11,6 +11,17 @@ func (s *chgkServer) WebHook(ctx context.Context, update *pb.Update) (*pb.Empty,
 	switch update.Message.Text {
 	case "/start":
 		_, err := s.RegisterUser(ctx, update.Message.From)
+		if err != nil && err.Error() == ErrUserAlreadyExists {
+			_, err := s.tg.SendMessage(update.Message.From.Id, fmt.Sprintf("Welcome back, %s!", update.Message.From.Username))
+			if err != nil {
+				return &pb.Empty{}, err
+			}
+			return &pb.Empty{}, nil
+		}
+		if err != nil {
+			return &pb.Empty{}, err
+		}
+		_, err = s.tg.SendMessage(update.Message.From.Id, fmt.Sprintf("Welcome, %s!", update.Message.From.Username))
 		if err != nil {
 			return &pb.Empty{}, err
 		}
