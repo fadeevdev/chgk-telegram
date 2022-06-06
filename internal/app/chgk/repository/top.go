73f44963e3ca2997c7cb89423da455a6d30f8f0c
dmitrii.fadeev <fadeevdev@gmail.com>
2022-06-06 21:51:53 +0300

    checking for error while in AddToTop repo method

--- internal/app/chgk/repository/top.go
+++ internal/app/chgk/repository/top.go
@@ -46,7 +46,7 @@ func (r *repository) AddToTop(ctx context.Context, uID uint64, qID uint64) (err 
 		) ON CONFLICT (id) do
 			update set answered_questions = array_prepend(answered_questions, $2);
 	`
-	r.pool.QueryRow(ctx, query,
+	_, err = r.pool.Exec(ctx, query,
 		uID,
 		qID,
 	)
