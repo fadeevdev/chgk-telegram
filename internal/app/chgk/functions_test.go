b2be5d62595af2ab83164ca551da136e8418562c
dmitrii.fadeev <fadeevdev@gmail.com>
2022-06-07 20:47:38 +0300

    cleaned repo

--- internal/app/chgk/functions_test.go
+++ internal/app/chgk/functions_test.go
@@ -1,17 +1,1 @@
 package chgk
-
-import "strings"
-
-func maskString(s string) string {
-	var sb strings.Builder
-	for _, s := range []rune(strings.ToLower(s)) {
-		if (s >= 'а' && s <= 'я') || (s >= 'a' && s <= 'z') {
-			sb.WriteRune('x')
-		} else if (s >= 'А' && s <= 'Я') || (s >= 'A' && s <= 'Z') {
-			sb.WriteRune('X')
-		} else {
-			sb.WriteRune(s)
-		}
-	}
-	return sb.String()
-}
