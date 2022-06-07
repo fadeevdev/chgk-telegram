c07ca6bee62b506404604a8c1034532b128f8871
dmitrii.fadeev <fadeevdev@gmail.com>
2022-06-07 11:31:31 +0300

    fix AddToTop pg.Array

--- internal/app/chgk/repository/top.go
+++ internal/app/chgk/repository/top.go
@@ -50,7 +50,7 @@ func (r *repository) AddToTop(ctx context.Context, uID uint64, qID uint64) (err 
 	`
 	_, err = r.pool.Exec(ctx, query,
 		uID,
-		pq.Array(qID),
+		pq.Array([]uint64{qID}),
 	)
 
 	fmt.Println(err)
