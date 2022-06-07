04789e1fc6d820e3a80f93cd9b91eaf43d4c74ca
dmitrii.fadeev <fadeevdev@gmail.com>
2022-06-07 19:42:28 +0300

    fix maskString function

--- internal/app/chgk/functions.go
+++ internal/app/chgk/functions.go
@@ -5,12 +5,12 @@ import "strings"
 func maskString(s string) string {
 	var sb strings.Builder
 	for _, s := range []rune(strings.ToLower(s)) {
-		if s >= 'а' && s <= 'я' {
+		if (s >= 'а' && s <= 'я') || (s >= 'a' && s <= 'z') {
 			sb.WriteRune('x')
-		} else if s >= 'А' && s <= 'Я' {
+		} else if (s >= 'А' && s <= 'Я') || (s >= 'A' && s <= 'Z') {
 			sb.WriteRune('X')
 		} else {
-			sb.WriteRune(rune(s))
+			sb.WriteRune(s)
 		}
 	}
 	return sb.String()
