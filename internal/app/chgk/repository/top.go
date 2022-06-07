320ebad1b3b86f4ad68071a11e2c48ff0a829bc1
dmitrii.fadeev <fadeevdev@gmail.com>
2022-06-07 09:39:15 +0300

    fix AddToTop repo sql syntax

--- internal/app/chgk/repository/top.go
+++ internal/app/chgk/repository/top.go
@@ -39,14 +39,13 @@ func (r *repository) GetTopPosition(ctx context.Context, uID uint64) (position m
 
 func (r *repository) AddToTop(ctx context.Context, uID uint64, qID uint64) (err error) {
 	const query = `
-		update correct_answers (
+		insert into correct_answers (
 			id,
 			answered_questions
 		) VALUES (
 			$1, '{$2}'
 		) ON CONFLICT (id) do
-			update correct_answers set answered_questions = array_prepend(answered_questions, $2)
-			where id = $1;
+			update set answered_questions = array_prepend(answered_questions, $2);
 	`
 	_, err = r.pool.Exec(ctx, query,
 		uID,
